fb2545c3b3fb16962842e1dc4be69f7f76a86881
vagrant <vagrant@localhost.localdomain>
2020-07-24 04:21:19 +0000

    [Update]books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,13 +12,35 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.fins(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(book.id), notice: 'Book was successfully crearted.'
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      redirect_to books_path, notice: 'Book was successfully destroyed.'
+    else
+      reder :index
+    end 
   end
 
   private
